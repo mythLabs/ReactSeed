bd45b4702dd4acb33722580f5e21efe559c86089
mythLabs <amitbisht744@gmail.com>
2018-07-29 10:49:23 +0530

    Added 3 environments files common,dev,prod

--- src/App.js
+++ src/App.js
@@ -8,8 +8,9 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">{process.env.REACT_APP_COMMON_NAME}</h1>
         </header>
+        <p className="App-intro">{process.env.REACT_APP_UNIQUE_NAME}</p>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
